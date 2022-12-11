a26ab03f5c3a76eab7c5dd0405bc2b295aa0fe9b
MaksymilianBilski <biger9@o2.pl>
2022-12-11 18:16:38 +0100

    j

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './Profile.module.css';
+import css from './profile.module.css';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => (
   <div className={css.profile}>
